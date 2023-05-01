3773909d8ae4106594b12f9b9351c88e40692549
MesakDuduCoder <duduaoa99@gmail.com>
2023-05-01 15:48:21 +0530

    Edit task funtion added

---
+++ src/Edits.js
@@ -0,0 +1,27 @@
+import { display } from './Display.js';
+
+export const editItem = (item) => {
+  const writeEdit = document.createElement('input');
+  writeEdit.id = 'edit';
+  writeEdit.type = 'text';
+  writeEdit.value = item.innerHTML;
+  item.innerHTML = '';
+  item.parentNode.insertBefore(writeEdit, item.nextSibling);
+};
+
+export const saveEdit = (item) => {
+  const lists = localStorage.getItem('toDoList');
+  const get = JSON.parse(lists);
+  let tasks = [];
+  if (get) {
+    tasks = get;
+  }
+
+  const newValue = document.querySelector('#edit');
+  tasks.forEach((taskItem) => {
+    if (item.description === taskItem.description) {
+      taskItem.description = newValue.value;
+    }
+  });
+  localStorage.setItem('toDoList', JSON.stringify(tasks));
+};
